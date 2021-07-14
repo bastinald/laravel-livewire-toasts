6925c8aa7ebfdd5309787ed47a889020b5acedd0
bastinald <bastinald@icloud.com>
2021-07-14 18:23:10 -0400

    fixed

--- resources/views/toasts.blade.php
+++ resources/views/toasts.blade.php
@@ -1,6 +1,6 @@
 <div
     id="laravel-livewire-toasts"
-    class="toast position-absolute align-items-center text-white bg-{{ $color }} border-0 fade hide"
+    class="toast position-fixed align-items-center text-white bg-{{ $color }} border-0 fade hide"
     style="top: 1rem; right: 1rem; width: 18rem; z-index: 1080;"
     data-bs-delay="{{ config('laravel-livewire-toasts.hide_delay') }}"
     data-error-message="{{ __(config('laravel-livewire-toasts.error_message')) }}">
