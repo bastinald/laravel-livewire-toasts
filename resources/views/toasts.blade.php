adc1a641af93c3a35d16e948a1e00269b08358b2
bastinald <bastinald@icloud.com>
2021-08-22 14:09:53 -0400

    z index tweak

--- resources/views/toasts.blade.php
+++ resources/views/toasts.blade.php
@@ -1,7 +1,7 @@
 <div
     id="laravel-livewire-toasts"
     class="toast position-fixed align-items-center text-white bg-{{ $color }} border-0 fade hide"
-    style="top: 1rem; right: 1rem; width: 18rem; z-index: 1080;"
+    style="top: 1rem; right: 1rem; width: 18rem; z-index: 1090;"
     data-bs-delay="{{ config('laravel-livewire-toasts.hide_delay') }}"
     data-error-message="{{ __(config('laravel-livewire-toasts.error_message')) }}">
     <div class="d-flex">
